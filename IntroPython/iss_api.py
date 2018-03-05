785cb65ea47c036ae10eb41f5aee0fb1cdcb429e
David W Grossman <2gotgrossman@gmail.com>
2018-03-04 19:24:40 -0500

    Script for getting the ISS position. Week 5 class

---
+++ IntroPython/iss_api.py
@@ -0,0 +1,34 @@
+import requests
+import time
+import json
+
+# Prints JSON with pizzazz
+def pretty_print(string):
+    parsed = json.loads(string)
+    return json.dumps(parsed, indent=4, sort_keys=True)
+
+# URL for REST API
+space_url = 'http://api.open-notify.org/iss-pass.json'
+
+# Parameters for the body of the REST API call
+params = {'lat' : 25.717396, 'lon':-80.278130}
+
+response = requests.get(space_url, params)
+
+print("Status Code:", response.status_code)
+if response.ok:
+    
+
+    print("Content:", pretty_print(response.content))
+
+    # Get date of next time we will see the ISS
+
+    parsed = json.loads(response.content)
+
+    if type(parsed) is dict and 'response' in parsed:
+        next_rise_time = parsed['response'][0]['risetime']
+        # Converts Unix Time to readable date
+        date_of_risetime = time.ctime(int(next_rise_time))
+        print("Next time we will see the ISS is", date_of_risetime)
+else:
+    print("ERROR: Bad response code. Exiting")
